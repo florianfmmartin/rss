d6260e180a2458fbe2621d7f22a896f1dbf1e224
florianfmmartin <florian.fm.martin@gmail.com>
2022-07-09 14:05:47 -0400

    :sparkles: Serve JSON feeds

--- index.ts
+++ index.ts
@@ -1,3 +1,23 @@
 import { serve } from "https://deno.land/std@0.142.0/http/server.ts";
+import { parseFeed } from "https://deno.land/x/rss/mod.ts";
 
-serve((req: Request) => new Response("Hello World"));
+const state = {
+  urls: [
+    "https://hnrss.org/best",
+    "https://journal.miso.town/atom?url=https://wiki.xxiivv.com/site/now.html",
+  ],
+};
+
+const getRSSFeeds = async (urls: string[]) => {
+  const responses = await Promise.all(urls.map((url) => fetch(url)));
+  const texts = await Promise.all(responses.map((response) => response.text()));
+  const feeds = await Promise.all(texts.map((text) => parseFeed(text)));
+  return feeds;
+};
+
+serve(
+  async (req: Request) =>
+    new Response(JSON.stringify(await getRSSFeeds(state.urls)), {
+      headers: { "Content-Type": "application/json" },
+    })
+);
